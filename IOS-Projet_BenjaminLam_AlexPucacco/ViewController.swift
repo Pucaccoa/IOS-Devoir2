8d24c161f8f6206dea5360c40860f68f54b41dba
alexpucacco <alexpucacco@alexpucaccos-imac.local>
2017-12-16 13:10:01 -0500

    Initial Commit

---
+++ IOS-Projet_BenjaminLam_AlexPucacco/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOS-Projet_BenjaminLam_AlexPucacco
+//
+//  Created by alexpucacco on 2017-12-16.
+//  Copyright © 2017 alexpucacco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
