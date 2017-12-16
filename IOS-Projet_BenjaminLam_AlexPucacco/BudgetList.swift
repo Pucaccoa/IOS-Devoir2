9572c57beb3efe4eaaa6f0f2b8a9d99fa2d69793
alexpucacco <alexpucacco@alexpucaccos-imac.local>
2017-12-16 16:04:51 -0500

    list fonctionne- detail a besoin d'un peu de travail

--- IOS-Projet_BenjaminLam_AlexPucacco/BudgetList.swift
+++ IOS-Projet_BenjaminLam_AlexPucacco/BudgetList.swift
@@ -13,21 +13,26 @@ import UIKit
 class BudgetList : UITableViewController
 {
     var list = ["budget1", "budget2","budget3"];
+    var cat = ["Food", "Shopping", "Hobby"];
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1; //Modifier
+        return list.count; //Modifier
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return list.count; //modifier ici
+        return 2; //modifier ici
     }
     
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return self.cat[section]
+    }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
             let cell = tableView.dequeueReusableCell(withIdentifier: "Budget",
             for : indexPath)
+        
             let budget   = list[indexPath.row];
         cell.textLabel?.text = budget
         cell.detailTextLabel?.text = "Test Budget"
@@ -37,5 +42,19 @@ class BudgetList : UITableViewController
         
         
     }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let idSegue  = segue.identifier;
+        if(idSegue == "Detail")
+        {
+            let cellule = sender as? UITableViewCell;
+            let index = tableView.indexPath(for: cellule!)?.row;
+            let destination  = segue.destination as? DetailBudgetController
+            let budgetTapote = list[index!];
+            destination?.budget = budgetTapote
+            print("Hello2")
+        }
+        
+    }
+    
     
 }
